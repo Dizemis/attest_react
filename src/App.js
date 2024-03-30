1f463adc964efe784fe14730c6ebfe8b99793ff4
Yura <45062968+Dizemis@users.noreply.github.com>
2024-03-31 03:25:13 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1>Аттестация по React</h1>
+        <p>Простейшая страница, включающая в себя:</p>
+        <ul>
+          <li>Заголовок</li>
+          <li>Абзац текста</li>
+          <li>Список</li>
+        </ul>
+      </div>
+  );
+}
+
+export default App;
